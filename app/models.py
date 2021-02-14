98a37b3738b5e06c7a7721c8246d23018dd7f6c5
Andrey Belskiy <diksandab@gmail.com>
2021-02-14 19:15:18 +0600

    Добавлен метод для модели User для обновления записи по user_id

--- app/models.py
+++ app/models.py
@@ -11,9 +11,7 @@ class User(db.Model):
     last_name = db.Column(db.String(50), nullable=False)
 
     def add_user(_first_name, _middle_name, _last_name):
-        """
-        Метод для создания нового пользователя.
-        """
+        """Метод для создания нового пользователя."""
         new_user = User(first_name=_first_name,
                         middle_name=_middle_name,
                         last_name=_last_name,)
@@ -22,8 +20,16 @@ class User(db.Model):
         return new_user
 
     def get_user(_user_id):
+        """Возвращает объект пользователя по user_id."""
         getting_user = User.query.get_or_404(_user_id)
         return getting_user
+
+    def update_user(_user_id, _first_name, _middle_name, _last_name):
+        upd_fields = {'first_name': _first_name,
+                        'middle_name': _middle_name,
+                        'last_name': _last_name,}
+        upd_user = User.query.filter_by(user_id=_user_id).update(upd_fields)
+        db.session.commit()
 
 
     def __repr__(self):
