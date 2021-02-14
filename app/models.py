cb10345694223f462dcb59c260f8ee5e28d6eb6e
Andrey Belskiy <diksandab@gmail.com>
2021-02-14 19:32:22 +0600

    Добавлен метод для перобразования объекта user в словарь. Метод get_user теперь возвращает словарь

--- app/models.py
+++ app/models.py
@@ -10,6 +10,13 @@ class User(db.Model):
     middle_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
 
+    def data_to_dict(self):
+        """Перобразует объект БД в словарь."""
+        return {'user_id': self.user_id,
+                'first_name': self.first_name,
+                'middle_name': self.middle_name,
+                'last_name': self.last_name}
+
     def add_user(_first_name, _middle_name, _last_name):
         """Метод для создания нового пользователя."""
         new_user = User(first_name=_first_name,
@@ -17,12 +24,12 @@ class User(db.Model):
                         last_name=_last_name,)
         db.session.add(new_user)
         db.session.commit()
-        return new_user
+        #return new_user
 
     def get_user(_user_id):
         """Возвращает объект пользователя по user_id."""
         getting_user = User.query.get_or_404(_user_id)
-        return getting_user
+        return User.data_to_dict(getting_user)
 
     def update_user(_user_id, _first_name, _middle_name, _last_name):
         """Обновляет поля для пользователя с user_id."""
