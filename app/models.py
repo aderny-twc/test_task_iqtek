afee2b6ceebddf1a558fa0e57c823a5c249e1534
Andrey Belskiy <diksandab@gmail.com>
2021-02-14 18:30:47 +0600

    Добавлен метод для создния нового пользователя. Перегружен метод стандартного отображения объекта модели.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,35 @@
 from app import db
+import json
 
 
 class User(db.Model):
+    """Модель пользователя."""
     __tablename__ = 'users'
     user_id = db.Column(db.Integer(), primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     middle_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
+
+    def add_user(_first_name, _middle_name, _last_name):
+        """
+        Метод для создания нового пользователя.
+        """
+        new_user = User(first_name=_first_name,
+                        middle_name=_middle_name,
+                        last_name=_last_name,)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+
+    def __repr__(self):
+        """Отображение пользователя в shell."""
+        user_obj = {
+                'user_id': self.user_id,
+                'first_name': self.first_name,
+                'middle_name': self.middle_name,
+                'last_name': self.last_name,
+                }
+        return json.dumps(user_obj)
+
+
