e4d911bbd7766f72f79eec284ec83d30e826a3d1
Andrey Belskiy <diksandab@gmail.com>
2021-02-15 20:58:30 +0600

    Обновлены ответы при обновлении и внесении данных (статус коды)

--- app/views.py
+++ app/views.py
@@ -15,24 +15,26 @@ def get_user(user_id):
 @app.route('/test/api/v0.1/user/', methods=['POST'])
 def create_user():
     """Создание нового объекта пользователя."""
-    if request.json and input_validation(request.json):
+    if input_validation(request.json):
         User.add_user(request.json['first_name'],
                         request.json['middle_name'],
                         request.json['last_name'])
 
-    return jsonify({'Response': 'User created'})
+    resp_body = jsonify({'Response': 'User created'})
+    return make_response(resp_body, 201)
 
 
 @app.route('/test/api/v0.1/user/<int:user_id>/', methods=['PUT'])
 def update_user(user_id):
     """Обновление объекта пользователя с user_id."""
-    if request.json and input_validation(request.json):
+    if input_validation(request.json):
         User.update_user(user_id,
                         request.json['first_name'],
                         request.json['middle_name'],
                         request.json['last_name'])
 
-    return jsonify({'Response': 'User updated'})
+    resp_body = jsonify({'Response': 'User updated'})
+    return make_response(resp_body, 200)
 
 
 @app.route('/test/api/v0.1/user/<int:user_id>/', methods=['DELETE'])
