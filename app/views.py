e0ffc2659ea1646a683e6222232176ea3cabe10d
Andrey Belskiy <diksandab@gmail.com>
2021-02-15 09:51:51 +0600

    Добавлена функция для возввращения ответа при отсутствии данных errorhandler 404. Добавлена тестовая функция валидации данных input_validation. Создана функция возвращения объекта пользователя по id.

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from app import app
-from flask import request
+from flask import request, jsonify, make_response
 from .models import User
 
 
 @app.route('/test/api/v0.1/user/<int:user_id>/', methods=['GET'])
 def get_user(user_id):
     """Получение объекта пользователя по user_id."""
-    pass
+    getting_user = User.get_user(user_id)
+    return jsonify(getting_user)
 
 
 @app.route('/test/api/v0.1/user/', methods=['POST'])
@@ -25,4 +26,22 @@ def update_user(user_id):
 def delete_user(user_id):
     """Удаление объекта пользователя c user_id."""
     pass
+
+
+#Возвращение ошибки при отстутствии данных
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
+
+def input_validation(json_obj):
+    """
+    Проверка входных данных. Принимает объект json.
+    """
+    user_fields = ['first_name', 'middle_name', 'last_name']
+    for field in user_fields:
+        if (field in json_obj
+            and type(json_obj[field]) != unicode):
+            abort(400)
+
 
